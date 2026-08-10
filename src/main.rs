d3a1284d9142ee5348c4ad34817b241c6ae53364
werener <lenyazhuk@yandex.ru>
2026-08-10 20:59:56 +0300

    feat: add base lexer class and implement integer and arithmetic tokenization with TDD

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use lang::ast::lexer::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut lexer = Lexer::new("12 + 3 * 4");
+    while let Some(token) = lexer.next_token() {
+        println!("{:?}",token.kind);
+    }
 }
