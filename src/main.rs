faa0043ed761f4a7f89b6f78632c4d4fc9cca88e
werener <lenyazhuk@yandex.ru>
2026-09-06 12:08:49 +0300

    style: choose name for the lang

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use lang::lexer::{lexer::*, token::Token};
+use periphery::lexer::{lexer::*, token::Token};
 
 fn main() {
     env_logger::init();
 
     let tokens = tokenize("100 + 50");
     // "100 + 50", Ok(vec![Integer(100), Plus, Integer(50), Eof]))
-    let res = lang::utils::print_difference(
+    let res = periphery::utils::print_difference(
         tokens.unwrap(),
         vec![
             Token::Integer(100),
